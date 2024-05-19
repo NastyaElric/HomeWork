3b3df34713e9c4f055c5b98f963bf106476ef2cf
NastyaElric <nastya.elric00@gmail.com>
2024-05-19 20:14:13 +0300

    Add files via upload

---
+++ 2.py
@@ -0,0 +1,37 @@
+#задача "Сложная сдача"
+cherry_cost = 34
+cherry_weight = 4.5
+monney = 500
+print(monney - cherry_weight * cherry_cost)
+#задача "Сдача всем"
+product_name = "клубника"
+product_cost = 560
+product_weight = 2
+monney_have = 2000
+print( monney_have - product_cost * product_weight)
+#задача "Работаем с выводом данных"
+product_name = 'Персики'
+product_cost =214
+product_weight =4
+monney_have =3000
+print("ЧЕК", product_name)
+print( "Вес: ", product_weight, "кг")
+print("Цена: ", product_cost, "руб/кг")
+print("Итого: ", product_cost * product_weight)
+print("Внесено: ", monney_have, "руб")
+print("Сдача: ", monney_have - product_cost * product_weight, "руб" )
+# задача "Самая простоя на свете"
+name = " купи конструктор!"
+number = 15
+print(name * number)
+#задача "Автоматизируем простоту"
+number = 6
+to_do = "код "
+words = "Обожаю писать "
+print( words, to_do * number)
+
+
+
+
+
+
