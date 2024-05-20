671fe166e68dd406afae2388c5e8103ce78417c1
NastyaElric <nastya.elric00@gmail.com>
2024-05-20 20:26:56 +0300

    Add files via upload
    Практическое задание по теме: "Неизменяемые и изменяемые объекты. Кортежи и списки"

---
+++ HM5.py
@@ -0,0 +1,8 @@
+immutable_var = 1, 2, "Мышь", "Винченцо", 2.3
+print(immutable_var)
+#immutable_var[0] = 1450
+#print(immutable_var) в кортежах нельзя поменять тип данных
+mutable_list = ["1961", "Film", "Dorama"]
+print(mutable_list)
+mutable_list[1] = "Apple"
+print(mutable_list)
