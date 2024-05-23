cc091221294c2c395dac4be6bf9f5c1b2136847e
NastyaElric <nastya.elric00@gmail.com>
2024-05-23 21:52:11 +0300

    Add files via upload
    Практическое задание по теме: "Списки и словари"

---
+++ HM6.py
@@ -0,0 +1,11 @@
+my_list = ["Apple", "banana", "coconut", "grape", "melon", 'peach']
+print("List: ", my_list)
+print('First and last elements: ', my_list[0:5:4])
+print(my_list[2:5:1])
+my_list[2] = "cherry"
+print( "Modified list: ", my_list)
+my_dict = {"Sun": "Солнце", "Sky": "Небо", "Moon": "Луна"}
+print("Dictionary: ", my_dict)
+print("Translation: ", my_dict["Sun"])
+my_dict["Moon"] = "Не луна"
+print("Modified dictionary: ",  my_dict)
