d90fecb454ff66a2fd1047dc28f56c7e069511c8
NastyaElric <nastya.elric00@gmail.com>
2024-05-25 18:10:30 +0300

    Update HM6.py

--- HM6.py
+++ HM6.py
@@ -1,6 +1,9 @@
 my_list = ["Apple", "banana", "coconut", "grape", "melon", 'peach']
 print("List: ", my_list)
-print('First and last elements: ', my_list[0:5:4])
+#print('First and last elements: ', my_list[0:5:4])
+# Далее следует исправление ошибки
+print("First element: ", my_list[0])
+print("Last element: ", my_list[-1])
 print(my_list[2:5:1])
 my_list[2] = "cherry"
 print( "Modified list: ", my_list)
