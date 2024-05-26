37c91bc19b940c792f873c84d3c94cc2e4dc3a4a
NastyaElric <nastya.elric00@gmail.com>
2024-05-26 17:30:40 +0300

    Add files via upload

---
+++ modul2_3.py
@@ -0,0 +1,7 @@
+my_list = [42, 69, 0, 322, 13, 0, 99, -5, 9, 8, 7, -6, 5]
+number_ = 0
+for i in range(len(my_list)):
+    while 1 > 0:
+        if number_ < my_list[i]:
+            print(my_list[i])
+        break
