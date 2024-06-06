709ab265b0270ca3d24839a0588e98cfcd3104ee
NastyaElric <nastya.elric00@gmail.com>
2024-06-06 21:16:24 +0300

    Update modul2_hard.py

--- modul2_hard.py
+++ modul2_hard.py
@@ -1,26 +1,11 @@
+def generate_password(n):
+    password = ''
+    for i in range(1, n - 1):
+        for j in range(i + 1, n + 1):
+            if n % (i + j) == 0:
+                password += str(i) + str(j)
+    return password
 
-import random
-
-def select():
-    num_ = [3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-    first_num = random.choice(num_)
-    return first_num
-
-
-first_num = select()
-print(first_num)
-
-for i in range(1, 21):
-    for j in range(2, 21):
-        sum_ = i + j
-        pairs = [i, j]
-        a = first_num
-        count = 0
-        if first_num % sum_ == 0:
-            count = count + 1
-            print(*pairs, end=" ")
-        else:
-            count = 0
-
-
-
+for n in range(3, 21):
+    password = generate_password(n)
+    print(f"{n} - {password}")
