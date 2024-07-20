d6ebe76969f6e71fd73438351be28dd22b619009
NastyaElric <nastya.elric00@gmail.com>
2024-07-20 11:48:32 +0300

    Update modul 7_1.py

--- modul 7_1.py
+++ modul 7_1.py
@@ -14,15 +14,14 @@ class Shop():
     def get_products(self):
         __file_name = 'products.txt'
         file = open(self.__file_name, "r")
-        return file.read()
         file.close()
 
     def add(self, *product):
-        __file_name = 'products.txt'
         for p in product:
-            if p not in product:
+            if p not in self.__file_name:
                 file = open(self.__file_name, "w")
                 file.write(p)
+                file.close()
             else:
                 print(f"Продукт {p.name} уже есть в магазине")
 
@@ -33,7 +32,7 @@ p1 = Product('Potato', 50.5, 'Vegetables')
 p2 = Product('Spaghetti', 3.4, 'Groceries')
 p3 = Product('Potato', 5.5, 'Vegetables')
 
-print(p2) # __str__
+print(p2)
 
 s1.add(p1, p2, p3)
 
