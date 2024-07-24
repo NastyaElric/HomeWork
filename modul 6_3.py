96b9d46d751b6d2633f0330ef43671744179422e
NastyaElric <nastya.elric00@gmail.com>
2024-07-24 12:49:50 +0300

    Update modul 6_3.py

--- modul 6_3.py
+++ modul 6_3.py
@@ -2,7 +2,6 @@ class Horse:
     def init(self, x_distance=0, sound='Frrr'):
         self.x_distance = x_distance
         self.sound = sound
-        super().init()
 
     def run(self, dx: int):
         self.x_distance += dx
@@ -13,14 +12,20 @@ class Eagle:
         self.y_distance = y_distance
         self.sound = sound
 
-    def fly(self, dy):
+
+    def fly(self, dy: int):
         self.y_distance += dy
 
 
 class Pegasus(Horse, Eagle):
+    
+    def __init__(self, x_distance):
+        super.__init__(x_distance)
     def move(self, dx, dy):
         self.run(dx)
         self.fly(dy)
+       
+
 
     def get_pos(self):
         return (self.x_distance, self.y_distance)
@@ -29,16 +34,14 @@ class Pegasus(Horse, Eagle):
         print(self.sound)
 
 
-if name == 'main':
-    p1 = Pegasus()
+p1 = Pegasus(0)
+p1.get_pos() == (0, 0)
+print(p1.get_pos())
+p1.move(10, 15)
+assert p1.get_pos() == (10, 15)
+print(p1.get_pos())
+p1.move(-5, 20)
+assert p1.get_pos() == (5, 35)
+print(p1.get_pos())
 
-    assert p1.get_pos() == (0, 0)
-    print(p1.get_pos())
-    p1.move(10, 15)
-    assert p1.get_pos() == (10, 15)
-    print(p1.get_pos())
-    p1.move(-5, 20)
-    assert p1.get_pos() == (5, 35)
-    print(p1.get_pos())
-
-    p1.voice()
+p1.voice()
